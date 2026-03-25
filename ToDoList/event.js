162c42e8986e9db4c0a0d0b78d0f0b12a7feac9c
Jhonatan <fc75475927@gmail.com>
2026-03-25 09:51:56 -0400

    feat: avance en clase

---
+++ ToDoList/event.js
@@ -0,0 +1,11 @@
+function agregarTarea(boton) {
+    const inputTarea = document.getElementById("input-tarea");
+    const textoTarea = inputTarea.value.trim();
+    if (textoTarea === "") {
+        return;
+    }
+    const lista=document.getElementById("tareas");
+    div.className="tarea";
+    div.innerHTML = ${texto}<span style: "color:green;"> l <span/>;
+    
+}
