70358497ed3d675bcba437a3edf99d141ae886c9
Jhonatan <fc75475927@gmail.com>
2026-05-26 21:08:13 -0400

    feat: agregacion de phatch

--- ToDoListV2/Backend/routes/tareas.routes.js
+++ ToDoListV2/Backend/routes/tareas.routes.js
@@ -4,16 +4,19 @@ const router = express.Router();
 
 const {
   obtenerTareas,
+  obtenerTareaPorId,
   crearTarea,
-  eliminarTarea,
   editarTarea,
+  eliminarTarea,
 } = require("../controllers/tareas.controller");
 
 router.get("/", obtenerTareas);
 
+router.get("/:id", obtenerTareaPorId);
+
 router.post("/", crearTarea);
 
-router.put("/:id", editarTarea);
+router.patch("/:id", editarTarea);
 
 router.delete("/:id", eliminarTarea);
 
