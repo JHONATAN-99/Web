40f1a55f3108a742fac81a024d525c5a7e957ae2
Jhonatan <fc75475927@gmail.com>
2026-05-26 20:47:23 -0400

    fix:puerto ip

--- ToDoListV2/Backend/server.js
+++ ToDoListV2/Backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dns = require("dns");
 require("dotenv").config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
 
