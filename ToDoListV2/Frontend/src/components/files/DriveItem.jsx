71ec4b2b0193bd38a0bd5fb6839c072f15f4417a
lets04 <letiloredosalazar2004@hotmail.com>
2026-06-02 20:57:27 -0400

    feat: crear componente para visualización de los archivos

---
+++ ToDoListV2/Frontend/src/components/files/DriveItem.jsx
@@ -0,0 +1,38 @@
+function DriveItem({
+  archivo,
+  onDelete,
+  onDownload,
+}) {
+  return (
+    <div className="file-item">
+      <span>
+        {archivo.nombreOriginal}
+      </span>
+
+      <div className="file-buttons">
+        <button
+          onClick={() =>
+            onDownload(
+              archivo._id
+            )
+          }
+        >
+          Descargar
+        </button>
+
+        <button
+          className="delete"
+          onClick={() =>
+            onDelete(
+              archivo._id
+            )
+          }
+        >
+          Eliminar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DriveItem;
