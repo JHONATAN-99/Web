9fb7dbe1f648f06dc4e84167b99a740c709aa0a1
lets04 <letiloredosalazar2004@hotmail.com>
2026-06-02 20:56:30 -0400

    feat: agregar servicio para gestión de archivos del drive

--- ToDoListV2/Frontend/src/services/archivosApi.js
+++ ToDoListV2/Frontend/src/services/archivosApi.js
@@ -1,51 +1,45 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/tareas";
+const API =
+  "http://localhost:3000/api/archivos";
 
-export const obtenerArchivos = async (tareaId) => {
-  const res = await axios.get(
-    `${API}/${tareaId}/archivos`
-  );
+export const obtenerArchivos =
+  async () => {
+    const res =
+      await axios.get(API);
 
-  return res.data;
-};
+    return res.data;
+  };
 
-export const subirArchivo = async (
-  tareaId,
-  archivo
-) => {
-  const formData = new FormData();
+export const subirArchivo =
+  async (archivo) => {
+    const formData =
+      new FormData();
 
-  formData.append("archivo", archivo);
+    formData.append(
+      "archivo",
+      archivo
+    );
 
-  const res = await axios.post(
-    `${API}/${tareaId}/archivos`,
-    formData,
-    {
-      headers: {
-        "Content-Type":
-          "multipart/form-data",
-      },
-    }
-  );
+    const res =
+      await axios.post(
+        API,
+        formData
+      );
 
-  return res.data;
-};
+    return res.data;
+  };
 
-export const eliminarArchivo = async (
-  tareaId,
-  archivoId
-) => {
-  await axios.delete(
-    `${API}/${tareaId}/archivos/${archivoId}`
-  );
-};
+export const eliminarArchivo =
+  async (id) => {
+    await axios.delete(
+      `${API}/${id}`
+    );
+  };
 
-export const descargarArchivo = (
-  tareaId,
-  archivoId
-) => {
-  window.open(
-    `${API}/${tareaId}/archivos/${archivoId}/download`
-  );
-};
+export const descargarArchivo =
+  (id) => {
+    window.open(
+      `${API}/${id}/download`
+    );
+  };
