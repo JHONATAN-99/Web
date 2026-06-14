c269672bafff3b6b2901d5f8dc00d8040cabc66d
Jhonatan <fc75475927@gmail.com>
2026-06-14 18:30:53 -0400

    feat: modelo realcionado con el usuario

--- ToDoListV2/Backend/models/Tarea.js
+++ ToDoListV2/Backend/models/Tarea.js
@@ -12,6 +12,12 @@ const tareaSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: true,
+    },
   },
   {
     timestamps: true,
@@ -19,3 +25,4 @@ const tareaSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Tarea", tareaSchema);
+
